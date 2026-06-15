74e85963f397b3ac24e87c5f5cd0253172803e48
Secured Node Dynamics <artyomjanzen3@gmail.com>
2026-06-15 05:19:06 -0700

    chore: release v2.10.2

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 LOG_LEVEL="${DOWNTIFY_LOG_LEVEL:-info}"
 VERSION_FILE="${DOWNTIFY_VERSION_FILE:-/data/app-version}"
 
-if [ "${DOWNTIFY_AUTO_BUMP_VERSION:-1}" = "1" ]; then
+if [ "${DOWNTIFY_AUTO_BUMP_VERSION:-0}" = "1" ]; then
     BASE_VERSION="$(python -c 'from downtify import __version__; print(__version__)')"
     python -m downtify.versioning --base "${BASE_VERSION}" --file "${VERSION_FILE}" --bump
 fi
